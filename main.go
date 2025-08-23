53054ad89c26b6db028c3a3a94c95da85a949c19
Harald Sitter <sitter@kde.org>
2025-08-23 04:41:08 +0200

    chunk in the same fashion as the remote
    should yield more stable results I hope... though TBH we should just
    download the caibx as well

--- main.go
+++ main.go
@@ -73,7 +73,8 @@ func file(c *gin.Context) {
 		index, err := erofsStore.GetIndex(erofs + ".caibx")
 		if err != nil {
 			desync.Log.Warn("Failed to get index for", erofs, ":", err)
-			index, _, err = desync.IndexFromFile(c, erofsPath, 32, 16*1024, 64*1024, 256*1024, desync.NewProgressBar("Chunking "))
+			// FIXME hardcoed 32
+			index, _, err = desync.IndexFromFile(c, erofsPath, 32, remoteIndex.Index.ChunkSizeMin, remoteIndex.Index.ChunkSizeAvg, remoteIndex.Index.ChunkSizeMax, desync.NewProgressBar("Chunking "))
 			if err != nil {
 				desync.Log.Warn("Failed to create index for", erofs, ":", err)
 				continue
